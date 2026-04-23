ce84aacb4410372d187e2d3dc97b20283e09e546
JY-Mar <mjyjy@outlook.com>
2026-04-23 14:52:07 +0800

    .

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,3 @@
-/*
- * @Author       : JY-Mar mjyjy@outlook.com
- * @Date         : 2026-04-15 18:06:19
- * @LastEditTime : 2026-04-15 18:06:20
- * @LastEditors  : JY-Mar mjyjy@outlook.com
- * @Description  : 文件描述
- */
 const config = {
   arrowParens: 'always',
   singleQuote: true,
